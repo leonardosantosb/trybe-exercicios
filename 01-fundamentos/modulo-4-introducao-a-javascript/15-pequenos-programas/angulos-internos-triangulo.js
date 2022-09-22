72413cf473ac2636035e7d009db8f95455f12074
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-22 18:35:05 +0100

    angulos do triangulo

--- 01-fundamentos/modulo-4-introducao-a-javascript/15-pequenos-programas/angulos-internos-triangulo.js
+++ 01-fundamentos/modulo-4-introducao-a-javascript/15-pequenos-programas/angulos-internos-triangulo.js
@@ -1,15 +1,15 @@
 const primeiroAngulo = 100;
 const segundoAngulo = 40;
-const terceiroAngulo = 40;
+const terceiroAngulo = -2;
 
 const teste = primeiroAngulo + segundoAngulo + terceiroAngulo;
-const resultado = teste === 180;
 
-if (resultado == 'true' && primeiroAngulo > 0 && segundoAngulo > 0 && terceiroAngulo >0) {
+if (primeiroAngulo < 0 || segundoAngulo < 0 || terceiroAngulo < 0) {
+    console.log("erro, coloque todos os angulos positivos")
+
+}else if (teste === 180 && primeiroAngulo > 0 && segundoAngulo > 0 && terceiroAngulo >0) {
  console.log('true')
-}
-else if (resultado == 'false') {
+
+}else{
     console.log('false')
 }
-else (primeiroAngulo < 0 || segundoAngulo < 0 || terceiroAngulo < 0)
-    console.log("erro, coloque todos os angulos positivos")
