b12cfac7e13efb1519e254dddf55d239c517267e
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-24 23:17:40 +0100

    array de 1 a 25

---
+++ 01-fundamentos/modulo-4-introducao-a-javascript/ Dia-02-javaScript-Array-e-loop-For/Lidando-com-arrays/divisao-de-cada-elemento-por-2.js
@@ -0,0 +1,11 @@
+//imprima o resultado da divisão de cada um dos elementos por 2.
+
+let numeros = [1,  2,  3,  4,  5,  6,  7,  8, 9, 10, 11, 12, 13, 14, 15, 16, 17, 18, 19, 20, 21, 22, 23, 24, 25];
+
+let divisao;
+
+for (let valores of numeros){
+    divisao = valores / 2;
+
+    console.log(divisao)
+}
