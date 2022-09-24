ab9bdc123330ce53a2cd5e530d0e392c8a1877d6
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-24 19:18:50 +0100

    soma de 1 a 50

---
+++ 01-fundamentos/modulo-4-introducao-a-javascript/ Dia-02-javaScript-Array-e-loop-For/soma-de-1-a-50.js
@@ -0,0 +1,10 @@
+// 1 - Desenvolva um algoritmo que calcula a soma dos números 1 até 50 usando a estrutura condicional "for". Retorne a resposta no seguinte formato:
+// A soma total de 1 até 50 é: X
+
+
+let soma = 0;
+
+for (let indexx = 1; indexx <= 50 ; indexx +=1) {
+    soma = soma + indexx;
+}
+console.log("A soma total de 1 até 50 é: " + soma);
