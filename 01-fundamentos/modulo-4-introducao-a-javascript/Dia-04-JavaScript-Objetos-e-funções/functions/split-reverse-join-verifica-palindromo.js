ee729a4ad4b9f777c3eb3705e1ab93d62d2ed36c
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-09-27 15:55:41 +0100

    menor valor

--- 01-fundamentos/modulo-4-introducao-a-javascript/Dia-04-JavaScript-Objetos-e-funções/functions/split-reverse-join-verifica-palindromo.js
+++ 01-fundamentos/modulo-4-introducao-a-javascript/Dia-04-JavaScript-Objetos-e-funções/functions/split-reverse-join-verifica-palindromo.js
@@ -1,3 +1,5 @@
+//Crie uma função que receba uma string e retorne true se for um palíndromo, ou false, se não for.
+
 function verificaPalindromo (nome){
     let separandoEmArray = nome.split('');
     let revertendoArray = separandoEmArray.reverse();
