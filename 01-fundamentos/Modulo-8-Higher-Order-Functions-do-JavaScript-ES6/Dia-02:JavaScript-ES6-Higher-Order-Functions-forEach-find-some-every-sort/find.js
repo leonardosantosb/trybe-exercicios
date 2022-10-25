34a3ee2edfa76238ac6ac8de11e375ed31ad7ec6
leonardosantosb <leonardosantosbasilio@gmail.com>
2022-10-25 15:48:59 -0300

    Modulo-8

---
+++ 01-fundamentos/Modulo-8-Higher-Order-Functions-do-JavaScript-ES6/Dia-02:JavaScript-ES6-Higher-Order-Functions-forEach-find-some-every-sort/find.js
@@ -0,0 +1,10 @@
+//Utilize o find para retornar o primeiro número do array que é divisível por 3 e 5, caso ele exista:
+
+const numbers = [19, 21, 30, 3, 45, 22, 15];
+
+const findDivisibleBy3And5 = () => {
+  const valor = numbers.find((number) => number % 3 === 0 && number % 5 === 0)
+  return valor;
+};
+
+console.log(findDivisibleBy3And5());
